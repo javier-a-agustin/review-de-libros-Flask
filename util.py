79a4facc68896d266728a95e439868117c530fe4
Javier A. Fernandez <22.javier.fernandez@gmail.com>
2020-07-25 21:05:38 -0300

    readme

--- util.py
+++
@@ -1,5 +0,0 @@
-def uri():
-    return "postgres://qonbefcuvwnovv:6434e814993bf3337177a5c91bcce0d12a5e463d06d0ff9a755f5a31eac43ff6@ec2-34-195-169-25.compute-1.amazonaws.com:5432/dd208u6a29o6o8"
-
-def apiKey():
-    return "IG9rkbwzLWpv9zjuxiMgQ"
